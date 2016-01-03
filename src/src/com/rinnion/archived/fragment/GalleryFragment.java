07bf68a8ff366c1167902e8a7b57a3fdb02eb729
Lenovo @ home <r.tretjakov@gmail.com>
2016-01-04 01:30:49 +0300

    -- add gallery gridview with 2 item

--- src/src/com/rinnion/archived/fragment/GalleryFragment.java
+++ src/src/com/rinnion/archived/fragment/GalleryFragment.java
@@ -7,16 +7,22 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
+import android.database.MatrixCursor;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import com.rinnion.archived.R;
 
@@ -80,6 +86,25 @@ public class GalleryFragment extends Fragment {
 
         // вторая вкладка будет выбрана по умолчанию
         tabHost.setCurrentTabByTag("photo");
+
+        String[] columns = new String[]{"_id", "path"};
+        MatrixCursor mc = new MatrixCursor(columns);
+        mc.addRow(new Object[]{1, R.drawable.general_bg});
+        mc.addRow(new Object[]{2, R.drawable.st_lady_bg});
+        mc.addRow(new Object[]{3, R.drawable.st_open_bg});
+
+        GridView gv = (GridView) tabHost.findViewById(R.id.gtl_gv_photo);
+
+        String[] names = new String[]{"path"};
+        int[] to = new int[] {R.id.il_iv_image};
+        gv.setAdapter(new SimpleCursorAdapter(getActivity(), R.layout.image_layout, mc, names, to, 0 ){
+            @Override
+            public void setViewImage(ImageView v, String value) {
+                v.setImageResource(R.drawable.logo_splash_screen);
+            }
+        });
+
+
 
         return tabHost;
     }
